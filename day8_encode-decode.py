a8c1d6d180c9ce1641dc5d1ebf37b37e29ff0418
Jamie <44046853+JamieYeap@users.noreply.github.com>
2022-02-12 03:19:53 +0000

    add decode encode file

---
+++ day8_encode-decode.py
@@ -0,0 +1,30 @@
+# add another set of alphabets to complete the circle
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+# to encode
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text+=new_letter
+    print (f"The encoded text is {cipher_text}")
+
+# to decode 
+def decrypt (plain_text,shift_amount):
+    real_text = ""
+    for letter in plain_text:
+        position = alphabet.index (letter)
+        new_position = position - shift_amount
+        real_text += alphabet[new_position] 
+    print (f"The decoded text is {real_text}")
+  
+if direction == "encode":
+    encrypt (plain_text=text,shift_amount=shift)
+if direction == "decode":
+    decrypt (plain_text=text,shift_amount=shift)
